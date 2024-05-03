ac5550e0037705c1d08a6f6b8c54d6aa7a834abe
Davideca <69940892+davideca88@users.noreply.github.com>
2024-05-03 17:18:15 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,9 +1,29 @@
 -- Deca's ~/.config/nvim/init.lua
+-- what was I doing: redoing the keymaps. Thinking on a NERDtree and vim-plug alternative
 
--- Vim Polyglot
-vim.g.polyglot_disabled = 'autoindent'
+--[[
+CONTENTS
 
--- PLUGINS, using vim-plug
+1. Plugins (actually vim-plug)                                                                                              lua-plugins
+
+    -> Plugin configs                                                                                                       lua-plugin-config
+    -> Colorscheme (sonokai)                                                                                                lua-plugin-config-colorscheme
+    -> Nvim-cmp                                                                                                             lua-plugin-config-cmp
+    -> Lualine                                                                                                              lua-plugin-config-lualine
+    -> Bufferline                                                                                                           lua-plugin-config-bufferline
+    -> NERDtree                                                                                                             lua-plugin-config-nerdtree
+    
+2. Options                                                                                                                  lua-options
+
+3. Keymaps                                                                                                                  lua-keymaps
+
+4. Legacy configs                                                                                                           lua-legacy
+]]--
+
+-- ####################################################################################################################################################################################################
+
+-- PLUGINS, using vim-plug                                                                                                  lua-plugins
+
 local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
@@ -32,11 +52,13 @@ vim.call('plug#begin')
 
 vim.call('plug#end')
 
+-- ####################################################################################################################################################################################################
 
--- nvim-cmp
+-- Plugin configs                                                                                                           lua-plugin-config
+
+-- nvim-cmp                                                                                                                 lua-plugin-config-cmp
 
 local cmp = require('cmp')
-
 local kind_icons = {
     Text = "",
     Method = "󰆧",
@@ -69,11 +91,11 @@ local kind_icons = {
 cmp.setup({
 
     snippet = {
-      -- REQUIRED - you must specify a snippet engine
-      expand = function(args)
-         require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-      -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
-      end,
+        -- REQUIRED - you must specify a snippet engine
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+            -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+        end,
     },
     
     mapping = cmp.mapping.preset.insert({
@@ -85,7 +107,6 @@ cmp.setup({
 
         ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
         ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
-
     }),
      
     
@@ -94,7 +115,8 @@ cmp.setup({
         format = function(entry, vim_item)
         -- Kind icons
         vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-     -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatenates the icons with the name of the item kind
+        -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatenates the icons with the name of the item kind
+        
         -- Source
         vim_item.menu = ({ 
             nvim_lsp = "[LSP]",
@@ -123,38 +145,36 @@ cmp.setup({
         ghost_text = true,
     }
 
-})
+}) -- end of cmp.setup
 
+-- LSP configs (lsp-config and cmp_nvim_lsp)
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
---    require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
---        capabilities = capabilities
---  }
 require('lspconfig')['clangd'].setup {
     capabilities = capabilities
 }
 
+-- Signs for LSP on editor
 local signs = { Error = "", Warn = "", Hint = "", Info = "" }
 for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
--- Colorscheme 
+-- Colorscheme                                                                                                              lua-plugin-config-colorscheme 
 vim.cmd.colorscheme('sonokai')
 vim.g.sonokai_style = 'atlantis'
-vim.g.sonokai_better_performance = 1
+vim.g.sonokai_better_performance = 0
 vim.g.sonokai_transparent_background = 0 -- options: 0, 1, 2
 
 
--- Lualine
+-- Lualine                                                                                                                  lua-plugin-config-lualine
 require('lualine').setup({
     options = {
         theme = 'sonokai'
     }
 })
 
--- Bufferline
+-- Bufferline                                                                                                               lua-plugin-config-bufferline
 require('bufferline').setup{
     options = {
         theme = 'sonokai',
@@ -162,35 +182,21 @@ require('bufferline').setup{
     }
 }
 
--- NERDTree
+-- NERDTree                                                                                                                 lua-plugin-config-nerdtree
 -- Use <C-o> to open/close NERDTree
-vim.cmd([[
-    autocmd BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif
-    nnoremap <C-o> :NERDTreeToggle<CR>
-]])
+vim.cmd.autocmd([[ BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif ]])
+vim.cmd([[ nnoremap <C-o> :NERDTreeToggle<CR> ]])
 
-vim.cmd([[
-    nnoremap q          :quit<CR>
-    nnoremap w          :write<CR>
-    nnoremap x          :wq<CR>
-    nnoremap <S-q>      :q!<CR>
-    nnoremap <C-p>      :source<CR>
-    nnoremap <C-right>  :bn<CR>
-    nnoremap <C-left>   :bp<CR>
-    nnoremap <C-del>    :bd<CR>
-    map      <C-l>      <C-w>l
-    map      <C-k>      <C-w>k
-    map      <C-j>      <C-w>j
-    map      <C-h>      <C-w>h
-]])
+-- ####################################################################################################################################################################################################
 
--- vim.cmd.colorscheme('sonokai')
+-- Options                                                                                                                  lua-options
 
 vim.opt.termguicolors = true
 vim.opt.showmode = false           -- Remove modes on prompt (useful with *line like plugins)
 vim.opt.confirm = true             -- Confirm saves
 vim.opt.wildmenu = true
-vim.opt.number = true              -- Line numbers
+vim.opt.number = true              -- Number lines
+vim.opt.hlsearch = false           -- Disable the highlight after search
 vim.opt.mouse = "a"                -- Enable mouse
 
 vim.cmd.filetype('indent on')  -- <tab> options
@@ -202,14 +208,48 @@ vim.opt.expandtab = true
 vim.opt.autoindent = true   -- Autoindent
 vim.opt.cpoptions = vim.opt.cpoptions + "I"
 
-vim.cmd.syntax('on')    -- Syntax highlight
+vim.cmd.syntax('on')        -- Syntax highlight
 vim.opt.wrap = false        -- No wrap the line
 vim.opt.modelines = 0       -- Security
 vim.opt.cursorline = true
 vim.opt.clipboard = "unnamedplus"
 
+-- ####################################################################################################################################################################################################
 
--- LEGACY
+-- Keymaps                                                                                                                  lua-keymaps
+local opts = { noremap = true, silent = true }
+keymap = vim.api.nvim_set_keymap
+
+-- Modes
+--   normal_mode = "n",
+--   command_mode = "c",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+
+-- Normal mode
+
+keymap('n', 'q', ':quit<CR>', opts)
+keymap('n', 'w', ':write<CR>', opts)
+keymap('n', 'x', ':wq<CR>', opts)
+keymap('n', '<S-q>', ':q!<CR>', opts)
+keymap('n', '<C-p>', ':source<CR>', opts)
+keymap('n', '<C-right>', ':bn<CR>', opts)
+keymap('n', '<C-left>', ':bp<CR>', opts)
+keymap('n', '<C-del>', ':bd<CR>', opts)
+keymap('n', '<C-l>', ':<C-w>l', opts)
+keymap('n', '<C-k>', ':<C-w>k', opts)
+keymap('n', '<C-j>', ':<C-w>j', opts)
+keymap('n', '<C-h>', ':<C-w>h', opts)
+
+-- Insert mode
+
+-- Visual mode
+
+-- ####################################################################################################################################################################################################
+
+-- LEGACY                                                                                                                   lua-legacy
 
 -- PLUGINS ----------------------------
 
@@ -221,6 +261,23 @@ vim.opt.clipboard = "unnamedplus"
 --    Plug ('ms-jpq/coq.artifacts', { ['branch'] = 'artifacts'} )
 
 ---------------------------------------
+
+-- Keymaps ----------------------------
+
+--vim.cmd([[
+--    nnoremap q          :quit<CR>
+--    nnoremap w          :write<CR>
+--    nnoremap x          :wq<CR>
+--    nnoremap <S-q>      :q!<CR>
+--    nnoremap <C-p>      :source<CR>
+--    nnoremap <C-right>  :bn<CR>
+--    nnoremap <C-left>   :bp<CR>
+--    nnoremap <C-del>    :bd<CR>
+--    map      <C-l>      <C-w>l
+--    map      <C-k>      <C-w>k
+--    map      <C-j>      <C-w>j
+--    map      <C-h>      <C-w>h
+--]])
 
 -- lsp-config
 -- require("lspconfig").ccls.setup{}

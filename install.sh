ff3343af5a7bb4f2c5327f3f4c924b4e35155757
Davideca <lopes.270206@gmail.com>
2026-09-16 12:29:06 +0000

    fix: remove paq-nvim as vim.pack is currently in use

--- install.sh
+++ install.sh
@@ -4,11 +4,6 @@ FONT_DIR="${HOME}/.fonts/"
 
 cat dotfiles/bashrc >> $HOME/.bashrc
 
-# Install NeoVim plugin manager (currently paq-nvim)
-mkdir -p $HOME/.local/share/
-git clone --depth=1 https://github.com/savq/paq-nvim.git \
-    "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/pack/paqs/start/paq-nvim
-
 # Create ~/.config and put init.lua
 mkdir -p $HOME/.config/nvim/
 cp dotfiles/init.lua $HOME/.config/nvim/init.lua
@@ -22,11 +17,6 @@ mkdir -p $FONT_DIR && \
 cp dotfiles/font.ttf $FONT_DIR && \
 fc-cache -f && \
 fc-cache -r
-
-# If has NeoVim, installs the plugins
-if nvim -v &> /dev/null; then
-    nvim --headless -c ':PaqInstall | echo "\nPlugins installed =D\n" | :q'
-fi
 
 # If wants to clean install dotfiles, remove the directory
 if [ "$1" = "clean" ]; then
